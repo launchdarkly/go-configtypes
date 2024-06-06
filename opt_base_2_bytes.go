58c54d1348f2133cf5a5e0de547c21c3118b7823
Matthew Keeler <mkeeler@launchdarkly.com>
2024-06-06 15:37:20 -0400

    feat: Add OptBase2Bytes type

---
+++ opt_base_2_bytes.go
@@ -0,0 +1,97 @@
+package configtypes
+
+import (
+	"encoding/json"
+
+	"github.com/alecthomas/units"
+	"gopkg.in/launchdarkly/go-sdk-common.v2/ldvalue"
+)
+
+// OptBase2Bytes represents an optional parameter which, if present, must be a
+// valid units.Base2Bytes.
+//
+// There is no additional validation. The value is stored as a
+// *units.Base2Bytes pointer, whose value is always copied when accessed; a nil
+// pointer indicates the lack of a value (the same as OptBase2Bytes{}).
+//
+// Converting to or from a string uses the standard behavior for
+// units.Base2Bytes.String() and units.ParseBase2Bytes().
+//
+// When converting to or from JSON, the value must be either a JSON null or a
+// JSON string.
+//
+// See the package documentation for the general contract for methods that have
+// no specific documentation here.
+type OptBase2Bytes struct {
+	size *units.Base2Bytes
+}
+
+func NewOptBase2Bytes(size units.Base2Bytes) OptBase2Bytes {
+	return OptBase2Bytes{size: &size}
+}
+
+func NewOptBase2BytesFromString(sizeAsString string) (OptBase2Bytes, error) {
+	if sizeAsString == "" {
+		return OptBase2Bytes{}, nil
+	}
+	size, err := units.ParseBase2Bytes(sizeAsString)
+	if err == nil {
+		return NewOptBase2Bytes(size), nil
+	}
+	return OptBase2Bytes{}, errBase2BytesFormat()
+}
+
+func (o OptBase2Bytes) IsDefined() bool {
+	return o.size != nil
+}
+
+func (o OptBase2Bytes) Get() *units.Base2Bytes {
+	return o.size
+}
+
+func (o OptBase2Bytes) String() string {
+	if o.size == nil {
+		return ""
+	}
+	return o.size.String()
+}
+
+func (o OptBase2Bytes) MarshalText() ([]byte, error) {
+	if o.size == nil {
+		return nil, nil
+	}
+	return []byte(o.String()), nil
+}
+
+func (o *OptBase2Bytes) UnmarshalText(data []byte) error {
+	parsed, err := NewOptBase2BytesFromString(string(data))
+	if err == nil {
+		*o = parsed
+	}
+	return err
+}
+
+func (o OptBase2Bytes) MarshalJSON() ([]byte, error) {
+	if o.size != nil {
+		return json.Marshal(o.size.String())
+	}
+	return json.Marshal(nil)
+}
+
+func (o *OptBase2Bytes) UnmarshalJSON(data []byte) error {
+	var v ldvalue.Value
+	var err error
+	if err = v.UnmarshalJSON(data); err != nil {
+		return err
+	}
+	switch {
+	case v.IsNull():
+		*o = OptBase2Bytes{}
+		return nil
+	case v.IsString():
+		*o, err = NewOptBase2BytesFromString(v.StringValue())
+		return err
+	default:
+		return errBase2BytesFormat()
+	}
+}
---
+++ opt_base_2_bytes_test.go
@@ -0,0 +1,67 @@
+package configtypes
+
+import (
+	"testing"
+
+	"github.com/alecthomas/units"
+	"github.com/stretchr/testify/assert"
+)
+
+const (
+	gigString           = "10GiB"
+	megString           = "3MiB"
+	malformedSizeString = "7gb"
+)
+
+var (
+	gigBytes, _ = units.ParseBase2Bytes(gigString)
+	megBytes, _ = units.ParseBase2Bytes(megString)
+)
+
+func TestOptBase2Bytes(t *testing.T) {
+	t.Run("empty value", func(t *testing.T) {
+		unsetValue := OptBase2Bytes{}
+		assertIsDefined(t, false, unsetValue)
+		assert.Nil(t, unsetValue.Get())
+	})
+
+	t.Run("defined value", func(t *testing.T) {
+		gigValue := NewOptBase2Bytes(gigBytes)
+		assertIsDefined(t, true, gigValue)
+		assert.Equal(t, gigBytes, *gigValue.Get())
+
+		megValue := NewOptBase2Bytes(megBytes)
+		assertIsDefined(t, true, megValue)
+		assert.Equal(t, megBytes, *megValue.Get())
+	})
+
+	stringCtor := func(input string) (interface{}, error) {
+		o, err := NewOptBase2BytesFromString(input)
+		return o, err
+	}
+
+	assertConvertToText(t, map[string]textMarshalerAndStringer{
+		"": OptBase2Bytes{}, gigString: NewOptBase2Bytes(gigBytes), megString: NewOptBase2Bytes(megBytes),
+	})
+
+	assertConvertFromText(t, &OptBase2Bytes{}, stringCtor, map[string]interface{}{
+		"": OptBase2Bytes{}, gigString: NewOptBase2Bytes(gigBytes), megString: NewOptBase2Bytes(megBytes),
+	})
+
+	assertConvertFromTextFails(t, &OptBase2Bytes{}, stringCtor, errBase2BytesFormat(),
+		malformedSizeString,
+	)
+
+	assertConvertToJSON(t, map[string]SingleValue{
+		`null`: OptBase2Bytes{}, quoteJSONString(gigString): NewOptBase2Bytes(gigBytes),
+		quoteJSONString(megString): NewOptBase2Bytes(megBytes),
+	})
+
+	assertConvertFromJSON(t, &OptBase2Bytes{}, map[string]interface{}{
+		`null`: OptBase2Bytes{}, quoteJSONString(gigString): NewOptBase2Bytes(gigBytes),
+		quoteJSONString(megString): NewOptBase2Bytes(megBytes),
+	})
+
+	assertConvertFromJSONFails(t, &OptBase2Bytes{},
+		`true`, `0.5`, quoteJSONString(malformedSizeString), `[]`, `{}`)
+}
